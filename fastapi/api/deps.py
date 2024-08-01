5c59e0f6d0d52aa4d1ac0faef954661869c222cb
cgbridgewater <cgbridgewater@outlook.com>
2024-07-31 17:58:28 -0700

    build out deps

--- fastapi/api/deps.py
+++ fastapi/api/deps.py
@@ -28,3 +28,16 @@ oath2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 oath2_bearer_dependency = Annotated[str, Depends(oath2_bearer)]
 
 async def get_current_user(token: oath2_bearer_dependency):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+        # if not all([username, user_id]):   #TRY THIS!!!
+        #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+        return {'username': username, 'id':user_id}
+    except JWTError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+    
+user_dependency = Annotated[dict, Depends(get_current_user)]
